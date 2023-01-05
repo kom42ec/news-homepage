597d170ef3e77757b180fe407d22c18558d8fd58
kom42ec <69169748+nikolakomazec@users.noreply.github.com>
2023-01-05 13:18:07 +0100

    remove menu when click on link

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ const headerEl = document.querySelector(".main-header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// Remove menu mobile when link clicked
+
+const navLink = document.querySelectorAll(".nav_list-link");
+
+navLink.forEach((n) =>
+  n.addEventListener("click", function () {
+    headerEl.classList.remove("nav-open");
+  })
+);
